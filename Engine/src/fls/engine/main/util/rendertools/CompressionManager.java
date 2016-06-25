ead64d8fe2f2e39e5b8a8fd685a8389bd29eebc9
h2n0 <elc954@hotmail.co.uk>
2016-06-25 12:47:28 +0100

    Added some helper functions for use with the Painter program

---
+++ Engine/src/fls/engine/main/util/rendertools/CompressionManager.java
@@ -0,0 +1,62 @@
+package fls.engine.main.util.rendertools;
+
+import java.util.Arrays;
+
+public class CompressionManager {
+
+	
+	public static int decompress(String s){
+		int res = 0;
+		if(s.equals("-1")){
+			res= -1;
+		}else res = Integer.parseInt(s, 16);
+		return res;
+	}
+	
+	public static int[] superDecompress(String s){
+		if(s.startsWith("#")){
+			int[] d = new int[8 * 8];
+			Arrays.fill(d, -1);// Make sure it's all transparent at the start
+			s = s.substring(1);
+			String[] secs = s.split(",");
+			int off = 0;// the possition in the array
+			for(int j = 0; j < secs.length; j++){
+				String c = secs[j].trim();
+				boolean multi = c.indexOf(":") != -1;
+				
+				int amt = -1;
+				int col = -1;
+				if(multi){
+					amt = Integer.parseInt(c.substring(0, c.indexOf(":")), 16);
+					col = Integer.parseInt(c.substring(c.indexOf(":")+1), 16);
+				}else{
+					col = Integer.parseInt(c, 16);
+					amt = 1;
+				}
+
+				//System.out.println(off + " : " + amt);
+				for(int i = 0; i < amt; i++){
+					d[off + i] = col;
+				}
+				
+				off += amt;
+			}
+			return d;
+		}else{
+			return null;
+		}
+	}
+	
+	public static int getVersion(String l){
+		if(l.startsWith("?")){
+			l = l.substring(1);
+			String data = l.substring(0, l.indexOf(":")).trim();
+			String value = l.substring(l.indexOf(":")+1).trim();
+			if(data.equals("Version")){
+				return Integer.parseInt(value);
+			}
+		}
+		
+		return -1;
+	}
+}
---
+++ Engine/src/fls/engine/main/util/rendertools/SpriteParser.java
@@ -0,0 +1,54 @@
+package fls.engine.main.util.rendertools;
+
+public class SpriteParser {
+
+	
+	private int[][] data;
+	
+	private int w;
+	
+	public SpriteParser(int s, String data){
+		this.w = s;
+		String[] lines = data.split("\n");
+		int compressionType = -1;
+		for(String line : lines){
+			if(line.startsWith("?") && compressionType == -1) {
+				int t = CompressionManager.getVersion(line);
+				if(t > -1){
+					compressionType = t;
+				}
+			}
+		}
+		this.data = new int[(s * s) * (s * s)][];
+		int dataPos = 0;
+		for(int i = 0; i < lines.length; i++){
+			String l = lines[i];
+			if(!l.startsWith("#"))continue;
+			l = l.substring(1);
+			String[] nd = l.trim().split(",");
+			int[] d = new int[s * s];
+			if(compressionType < 3){
+				for(int j = 0; j < nd.length; j++){
+					d[j] = CompressionManager.decompress(nd[j]);
+				}
+				this.data[dataPos] = d;
+			}else{
+				this.data[dataPos] = CompressionManager.superDecompress(lines[i]);
+			}
+			dataPos ++;
+		}
+	}
+	
+	public int[] getData(int x, int y){
+		int[] res = new int[this.w * this.w];
+		int tx = x;
+		int ty = y;
+		if(tx < 0 || ty < 0 || tx >= this.w  || ty >= this.w)return null;
+		for(int i = 0; i < this.w * this.w; i++){
+			int dx = i % this.w;
+			int dy = i / this.w;
+			res[dx + dy * this.w] = this.data[tx + ty * this.w][i];
+		}
+		return res;
+	}
+}
